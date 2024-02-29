1889deb0d91b0ab824ffedf5a82d4af439f2988b
KhaledSaleh122 <voxagolde2003@gmail.com>
2024-02-29 18:46:13 +0200

    implement DisplayProducts to view all products in inventory

--- SimpleInventoryManagementSystem/Domain/InventoryManagemnt/Inventory.cs
+++ SimpleInventoryManagementSystem/Domain/InventoryManagemnt/Inventory.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using SimpleInventoryManagementSystem.Domain.ProductManagement;
 namespace SimpleInventoryManagementSystem.Domain.InventoryManagemnt
 {
@@ -16,6 +18,20 @@ namespace SimpleInventoryManagementSystem.Domain.InventoryManagemnt
             Product product = new Product(name, price, quantity);
             products.Add(product);
             return product;
+        }
+        public static void DisplayProducts() {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Name  |  Price  |  Quantity\n");
+            Console.WriteLine("{0,-15} | {1,-10} | {2,-8}", "Name", "Price", "Quantity");
+            Console.WriteLine(new string('-', 40)); // Separator line
+            if (products.Count == 0) {
+                Console.WriteLine("No products in inventory yet");
+                return;
+            }
+            foreach (Product product in products)
+            {
+                Console.WriteLine("{0,-15} | {1,-10} | {2,-8}", product.Name, product.Price, product.Quantity);
+            }
         }
     }
 }
