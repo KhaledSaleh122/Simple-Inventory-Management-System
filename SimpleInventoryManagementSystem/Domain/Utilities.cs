24c8bded695a2e0bf43f8273ab775d60eb235a48
KhaledSaleh122 <voxagolde2003@gmail.com>
2024-02-29 15:49:44 +0200

    Add Show Main Menu in the interface This will allow user to selected among options to manage the inventory

--- SimpleInventoryManagementSystem/Domain/Utilities.cs
+++ SimpleInventoryManagementSystem/Domain/Utilities.cs
@@ -8,5 +8,41 @@ namespace SimpleInventoryManagementSystem.Domain
 {
     internal class Utilities
     {
+        internal static void ShowMainMenu()
+        {
+            String userInput;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Wellcome to Inventory Management System");
+                Console.WriteLine();
+                Console.WriteLine("**Select an Action from the Menu**");
+                Console.WriteLine();
+                Console.WriteLine("1: Add a product");
+                Console.WriteLine("2: View all products");
+                Console.WriteLine("3: Edit a product");
+                Console.WriteLine("4: Delete a product");
+                Console.WriteLine("5: Search for a product");
+                Console.WriteLine("0: Close the application");
+                userInput = Console.ReadLine();
+                switch (userInput) {
+                    case "1":
+                        ShowAddProductMenu();break;
+                    case "2":
+                        ShowAllProducts();break;
+                    case "3":
+                        ShowEditProductMenu();break;
+                    case "4":
+                        ShowDeleteProductMenu();break;
+                    case "5":
+                        ShowSearchForProductMenu();break;
+                    case "0":
+                        break;
+                    default:
+                        Console.WriteLine("You selected wrong option, Try again.");
+                        break;
+                }
+            } while (userInput != "0");
+        }
     }
 }
