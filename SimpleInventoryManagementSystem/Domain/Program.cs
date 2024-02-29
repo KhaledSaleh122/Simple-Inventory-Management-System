bd68c536e629a3c55f4038793cfda99924a7c063
KhaledSaleh122 <voxagolde2003@gmail.com>
2024-02-29 21:32:01 +0200

    entry

---
+++ SimpleInventoryManagementSystem/Domain/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SimpleInventoryManagementSystem.Domain.ProductManagement;
+using SimpleInventoryManagementSystem.Domain.InventoryManagemnt;
+using SimpleInventoryManagementSystem.Domain;
+Inventory.AddProduct("Suger", 22, 1);
+Inventory.AddProduct("milk", 30, 100);
+Utilities.ShowMainMenu();
