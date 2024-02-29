0461b74304ed5bf12cc48005b35acc671e422e73
KhaledSaleh122 <voxagolde2003@gmail.com>
2024-02-29 16:27:15 +0200

    implement ShowAddProductMenu it still need to implement check for name exist and create and add product to inventory

--- SimpleInventoryManagementSystem/Domain/Utilities.cs
+++ SimpleInventoryManagementSystem/Domain/Utilities.cs
@@ -29,13 +29,13 @@ namespace SimpleInventoryManagementSystem.Domain
                     case "1":
                         ShowAddProductMenu();break;
                     case "2":
-                        ShowAllProducts();break;
+                        //ShowAllProducts();break;
                     case "3":
-                        ShowEditProductMenu();break;
+                        //ShowEditProductMenu();break;
                     case "4":
-                        ShowDeleteProductMenu();break;
+                        //ShowDeleteProductMenu();break;
                     case "5":
-                        ShowSearchForProductMenu();break;
+                        //ShowSearchForProductMenu();break;
                     case "0":
                         break;
                     default:
@@ -43,6 +43,47 @@ namespace SimpleInventoryManagementSystem.Domain
                         break;
                 }
             } while (userInput != "0");
+        }
+        internal static void ShowAddProductMenu()
+        {
+            String userInput;
+            String name = String.Empty;
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("Enter The Product Name: (ENTER 0 TO CANCEL THE OPERATION)");
+                userInput = Console.ReadLine();
+                if (String.IsNullOrEmpty(userInput))
+                {
+                    Console.WriteLine("Invaild Input");
+                }
+                else if (userInput == "0")
+                {
+                    return;
+                }
+                //Check if name allready exist
+                name = userInput;
+            } while (String.IsNullOrEmpty(name));
+            int price = -1;
+            do
+            {
+                Console.WriteLine("Enter The Product Price: (ENTER 0 TO CANCEL THE OPERATION)");
+                userInput = Console.ReadLine();
+                bool sucess = int.TryParse(userInput, out int price_);
+                if (!sucess)
+                {
+                    Console.WriteLine("Invaild Input");
+                    continue;
+                }
+                price = price_;
+                if (userInput == "0")
+                {
+                    return;
+                }
+                if (price < 0) { 
+                    Console.WriteLine("Price must be bigger or equal to zero")
+                }
+            } while (price < 0);
         }
     }
 }
