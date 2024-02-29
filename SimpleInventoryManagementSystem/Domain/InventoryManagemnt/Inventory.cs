6eba6c5093fed3d1d4ab04138880766e9c1bb401
KhaledSaleh122 <voxagolde2003@gmail.com>
2024-02-29 17:20:35 +0200

    implement IsProductNameAlreadyUsed to make sure that no duplicate in name, because the name must be unique

--- SimpleInventoryManagementSystem/Domain/InventoryManagemnt/Inventory.cs
+++ SimpleInventoryManagementSystem/Domain/InventoryManagemnt/Inventory.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using SimpleInventoryManagementSystem.Domain.ProductManagement;
 namespace SimpleInventoryManagementSystem.Domain.InventoryManagemnt
 {
     internal class Inventory
     {
+        internal static List<Product> products = new List<Product>();
+        internal static bool IsProductNameAlreadyUsed(String name) { 
+            return products.Find((e) => e.Name == name) != null;
+        }
     }
 }
